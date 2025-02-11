5deddcd084a54605db1f3702bb6f20312bce228c
Thiago Farias <thygo10@icloud.com>
2025-02-10 21:36:47 -0300

    up

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link"
+
 export default function Header() {
   return (
     <header>
       <nav>
         <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/about">About</Link></li>
+          <li><Link href="/contact">Contact</Link></li>
         </ul>
       </nav>
     </header>
